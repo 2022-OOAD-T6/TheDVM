c01cbbb703a0005e5335427a7d35b529c68cb27a
kyuhyeongkwon <akcps1tp@naver.com>
2022-05-17 21:22:58 +0900

    Add method body

--- src/main/java/dvm/service/PrepaymentService.java
+++ src/main/java/dvm/service/PrepaymentService.java
@@ -3,47 +3,52 @@ package dvm.service;
 import dvm.domain.PrepaymentInfo;
 import dvm.repository.PrepaymentRepository;
 
-/**
- * 
- */
+import java.util.Random;
+
 public class PrepaymentService {
 
-    /**
-     * Default constructor
-     */
-    public PrepaymentService() {
+    private final PrepaymentRepository prepaymentRepository;
+    private final Random random;
+
+    public PrepaymentService(PrepaymentRepository prepaymentRepository) {
+        this.prepaymentRepository = prepaymentRepository;
+        this.random = new Random();
     }
 
     /**
-     * 
+     * 선결제 정보 확인
      */
-    private PrepaymentRepository prepaymentRepository;
-
-    /**
-     * @param verifiationCode 
-     * @return
-     */
-    public PrepaymentInfo getPrepaymentInfo(String verifiationCode) {
-        // TODO implement here
-        return null;
+    public PrepaymentInfo getPrepaymentInfo(String verificationCode) {
+        return prepaymentRepository.getPrepaymentInfo(verificationCode);
     }
 
     /**
-     * @param verificationCode 
-     * @param itemCode 
-     * @param quantity 
-     * @return
+     * 선결제 정보 저장
+     * 수정사항: 정상적인 선결제인지 지정하는 isValid 파라미터 추가
      */
-    public void savePrepaymentInfo(String verificationCode, String itemCode, int quantity) {
-        // TODO implement here
+    public void savePrepaymentInfo(String verificationCode, String itemCode, int quantity, boolean isValid) {
+        prepaymentRepository.save(verificationCode, new PrepaymentInfo(itemCode, quantity, isValid));
     }
 
     /**
-     * @return
+     * 숫자, 소문자를 반드시 포함하는 10자리 코드 생성
      */
     public String generateVerificationCode() {
-        // TODO implement here
-        return "";
+        int leftLimit = 48;
+        int rightLimit = 122;
+        int codeLength = 10;
+
+        while (true) {
+            String code = random.ints(leftLimit, rightLimit + 1)
+                    .filter(ascii -> (ascii <= 57 || ascii >= 97))
+                    .limit(codeLength)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+            if (code.matches("^.*[a-z].*$") && code.matches("^.*[0-9].*$")) {
+                return code;
+            }
+        }
+
     }
 
 }
