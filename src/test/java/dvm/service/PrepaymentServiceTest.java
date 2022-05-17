4f252572921b167533c5cfd08ffc0da30c006765
kyuhyeongkwon <akcps1tp@naver.com>
2022-05-17 21:34:35 +0900

    Add unit test code

--- src/test/java/dvm/service/PrepaymentServiceTest.java
+++ src/test/java/dvm/service/PrepaymentServiceTest.java
@@ -1,5 +1,6 @@
 package dvm.service;
 
+import dvm.domain.PrepaymentInfo;
 import dvm.repository.PrepaymentRepository;
 import org.junit.jupiter.api.Test;
 
@@ -11,10 +12,35 @@ class PrepaymentServiceTest {
 
     @Test
     void getPrepaymentInfo() {
+        PrepaymentInfo saveInfo = prepaymentService.getPrepaymentInfo("vCode1");
+        assertNull(saveInfo);
+
+        String vCode = "vCode1";
+        String iCode = "itemCode1";
+        int quantity = 20;
+        boolean valid = false;
+
+        prepaymentService.savePrepaymentInfo(vCode, iCode, quantity, valid);
+        saveInfo = prepaymentService.getPrepaymentInfo(vCode);
+
+        assertEquals(saveInfo.getItemCode(), iCode);
+        assertEquals(saveInfo.getQuantity(), quantity);
+        assertEquals(saveInfo.isValid(), valid);
     }
 
     @Test
     void savePrepaymentInfo() {
+        String vCode = "vCode2";
+        String iCode = "itemCode2";
+        int quantity = 10;
+        boolean valid = true;
+
+        prepaymentService.savePrepaymentInfo(vCode, iCode, quantity, valid);
+        PrepaymentInfo saveInfo = prepaymentService.getPrepaymentInfo(vCode);
+
+        assertEquals(saveInfo.getItemCode(), iCode);
+        assertEquals(saveInfo.getQuantity(), quantity);
+        assertEquals(saveInfo.isValid(), valid);
     }
 
     @Test
