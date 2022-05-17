f90db2f39774b5ca21d467557bea83cc893e361f
moon-zzyy <gracemjy@konkuk.ac.kr>
2022-05-17 21:58:12 +0900

    중간저장

--- src/main/java/dvm/service/ItemService.java
+++ src/main/java/dvm/service/ItemService.java
@@ -3,53 +3,29 @@ package dvm.service;
 import dvm.repository.ItemRepository;
 
 /**
- * 
+ * 20220517 문지영
  */
 public class ItemService {
 
-    /**
-     * Default constructor
-     */
     public ItemService() {
     }
 
-    /**
-     * 
-     */
     private ItemRepository itemRepository;
 
-    /**
-     * @param itemCode 
-     * @param quantity 
-     * @return
-     */
     public boolean isEnough(String itemCode, int quantity) {
         // TODO implement here
         return false;
     }
 
-    /**
-     * @param itemCode 
-     * @return
-     */
     public int getItemPrice(String itemCode) {
         // TODO implement here
         return 0;
     }
 
-    /**
-     * @param itemCode 
-     * @param quantity 
-     * @return
-     */
     public void updateStock(String itemCode, int quantity) {
         // TODO implement here
     }
 
-    /**
-     * @param itemCode 
-     * @return
-     */
     public int getItemCount(String itemCode) {
         // TODO implement here
         return 0;
