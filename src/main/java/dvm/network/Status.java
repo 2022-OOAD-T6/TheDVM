d3dcf0ad62d64299290641522f0e12e74a1561c7
kyuhyeongkwon <akcps1tp@naver.com>
2022-05-17 12:48:59 +0900

    enum Status modified

--- src/main/java/dvm/network/Status.java
+++ src/main/java/dvm/network/Status.java
@@ -4,26 +4,7 @@ package dvm.network;
  * 
  */
 public enum Status {
-
-    // /**
-    //  * Default constructor
-    //  */
-    // public Status() {
-    // }
-    //
-    // /**
-    //  *
-    //  */
-    // public void SALE;
-    //
-    // /**
-    //  *
-    //  */
-    // public void STOCK;
-    //
-    // /**
-    //  *
-    //  */
-    // public void STOP;
-
+    SALE,
+    STOCK,
+    STOP
 }
