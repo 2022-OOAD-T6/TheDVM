fae9c9ef97c055bc8c9b08e7a51b3b2cdaa8d81c
kyuhyeongkwon <akcps1tp@naver.com>
2022-05-17 21:22:05 +0900

    Add method body

--- src/main/java/dvm/repository/PrepaymentRepository.java
+++ src/main/java/dvm/repository/PrepaymentRepository.java
@@ -2,39 +2,32 @@ package dvm.repository;
 
 import dvm.domain.PrepaymentInfo;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
-/**
- * 
- */
 public class PrepaymentRepository {
 
-    /**
-     * Default constructor
-     */
+    private final ConcurrentHashMap<String, PrepaymentInfo> savedPrepayments;
+
     public PrepaymentRepository() {
+        savedPrepayments = new ConcurrentHashMap<>();
     }
 
     /**
-     * 
-     */
-    private HashMap<String, PrepaymentInfo> savedPrepayments;
-
-    /**
-     * @param verificationCode 
-     * @return
+     * 인증코드와 일치하는 음료 정보 리턴
      */
     public PrepaymentInfo getPrepaymentInfo(String verificationCode) {
-        // TODO implement here
-        return null;
+        PrepaymentInfo info = savedPrepayments.get(verificationCode);
+        if (info != null) {
+            savedPrepayments.remove(verificationCode);
+        }
+        return info;
     }
 
     /**
-     * @param prepaymentInfo 
-     * @return
+     * 외부에서 받은 인증코드 저장
      */
-    public void save(PrepaymentInfo prepaymentInfo) {
-        // TODO implement here
+    public void save(String verificationCode, PrepaymentInfo prepaymentInfo) {
+        savedPrepayments.put(verificationCode, prepaymentInfo);
     }
 
 }
