2483a5e3b6ca55a41789ae13187a737ede079fa4
ggyul <wakegyul@gmail.com>
2022-05-21 16:38:37 +0900

    test: 한 메소드에서 테스트
    socket bind 오류로 인함

--- src/test/java/dvm/network/NetworkServiceTest.java
+++ src/test/java/dvm/network/NetworkServiceTest.java
@@ -19,23 +19,21 @@ class NetworkServiceTest {
     private final Serializer serializer = new Serializer();
 
     @Test
-    void getSaleResponseMessage() throws InterruptedException {
+    void getResponseMessage() throws InterruptedException {
         networkService.changeWaitingMessageType(MessageType.SALE_RESPONSE);
         networkService.sendSaleResponseMessage("Team6", "05");
         Thread.sleep(1000);
         Message message = networkService.getSaleResponseMessage();
         assertThat(serializer.message2Json(message)).isEqualTo("{\"srcId\":\"Team6\",\"dstID\":\"Team6\",\"msgType\":\"SalesCheckResponse\",\"msgDescription\":{\"itemCode\":\"05\",\"itemNum\":0,\"dvmXCoord\":10,\"dvmYCoord\":10}}");
-    }
 
-    @Test
-    void getStockResponseMessageFrom() throws InterruptedException {
         networkService.changeWaitingMessageType(MessageType.STOCK_RESPONSE);
         MessageFactory.setCurrentId("Team4");
         networkService.sendStockResponseMessage("Team6", "05", 15);
         MessageFactory.setCurrentId("Team3");
         networkService.sendStockResponseMessage("Team6", "05", 20);
         Thread.sleep(1000);
-        Message message = networkService.getStockResponseMessageFrom("Team3");
+        message = networkService.getStockResponseMessageFrom("Team3");
         assertThat(serializer.message2Json(message)).isEqualTo("{\"srcId\":\"Team3\",\"dstID\":\"Team6\",\"msgType\":\"StockCheckResponse\",\"msgDescription\":{\"itemCode\":\"05\",\"itemNum\":20,\"dvmXCoord\":10,\"dvmYCoord\":10}}");
     }
+
 }
