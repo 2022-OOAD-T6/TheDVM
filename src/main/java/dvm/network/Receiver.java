0b01dbcb1dbeff09f3115f4e596a865b66d3aa5f
ggyul <wakegyul@gmail.com>
2022-05-23 17:02:45 +0900

    WIP: DVM Server로 변경

--- src/main/java/dvm/network/Receiver.java
+++ src/main/java/dvm/network/Receiver.java
@@ -1,5 +1,6 @@
 package dvm.network;
 
+import DVM_Server.DVMServer;
 import Model.Message;
 import dvm.service.ItemService;
 import dvm.service.PrepaymentService;
@@ -25,42 +26,61 @@ public class Receiver implements Runnable {
 
     private MessageType waitingMessageType;
 
+    private DVMServer dvmServer;
+
+    private ReceiveMessageHandler receiveMessageHandler;
+
     public Receiver(ItemService itemService, PrepaymentService prepaymentService, NetworkService networkService) {
         this.itemService = itemService;
         this.prepaymentService = prepaymentService;
         this.networkService = networkService;
         this.responseMessages = new Vector<>();
         this.waitingMessageType = MessageType.NONE;
+        receiveMessageHandler = new ReceiveMessageHandler(itemService, prepaymentService, networkService, dvmServer);
+        new Thread(receiveMessageHandler).start();
+
     }
 
     @Override
     public void run() {
-        while(true){
-            try (ServerSocket serverSocket = new ServerSocket(port)) {
-                Socket socket = serverSocket.accept();
-                ReceiveMessageHandler handler = new ReceiveMessageHandler(socket, waitingMessageType, responseMessages, itemService, prepaymentService, networkService);
-                new Thread(handler).start();
-            } catch (Exception e) {
+
+            dvmServer = new DVMServer();
+            //receiveMessageHandler = new ReceiveMessageHandler(itemService, prepaymentService, networkService, dvmServer);
+
+            try{
+                dvmServer.run();
+            }catch(Exception e){
                 e.printStackTrace();
             }
-        }
+            // try (ServerSocket serverSocket = new ServerSocket(port)) {
+            //     Socket socket = serverSocket.accept();
+            //     ReceiveMessageHandler handler = new ReceiveMessageHandler(socket, waitingMessageType, responseMessages, itemService, prepaymentService, networkService);
+            //     new Thread(handler).start();
+            // } catch (Exception e) {
+            //     e.printStackTrace();
+            // }
+
+
+
+
     }
 
     public void changeWaitingMessageType(MessageType messageType) {
-        this.waitingMessageType = messageType;
-        if(messageType == MessageType.NONE){
-            System.out.println("receiver 상태 수정 | 기다리는 메세지가 없습니다. ");
-        }else{
-            System.out.println("receiver 상태 수정 | " + waitingMessageType.getTypeName() + "을 기다리는 중");
-        }
+        receiveMessageHandler.changeWaitingMessageType(messageType);
+        // this.waitingMessageType = messageType;
+        // if(messageType == MessageType.NONE){
+        //     System.out.println("receiver 상태 수정 | 기다리는 메세지가 없습니다. ");
+        // }else{
+        //     System.out.println("receiver 상태 수정 | " + waitingMessageType.getTypeName() + "을 기다리는 중");
+        // }
     }
 
     public Vector<Message> getResponseMessages() {
-        return responseMessages;
+        return receiveMessageHandler.getResponseMessages();
     }
 
     public void clearResponseMessages() {
-        responseMessages.clear();
+        receiveMessageHandler.clearResponseMessages();
     }
 
 }
