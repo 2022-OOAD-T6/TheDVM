14b7c0fb72e0494bdaa6a4b511767a5309d7bc85
kyuhyeongkwon <akcps1tp@naver.com>
2022-05-23 13:03:37 +0900

    Add AdminPanel body

--- src/main/java/dvm/gui/AdminPanel.java
+++ src/main/java/dvm/gui/AdminPanel.java
@@ -1,9 +1,12 @@
 package dvm.gui;
 
 import dvm.controller.Controller;
+import dvm.domain.Item;
+import dvm.domain.Response;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 /**
  * 관리자 화면
@@ -25,14 +28,24 @@ public class AdminPanel extends JPanel {
     JButton subBtn = new JButton("SUB");// 빼기 버튼
     JButton addBtn = new JButton("ADD");// 넣기 버튼
 
+    int userSelectionIndex;
+    int userSelectionQuantity;
+    JPanel[] typePanel;
+    JLabel selectedItem = new JLabel();
+    List<Item> ourItems;
+
     public AdminPanel(Controller controller){
         this.controller = controller;
 
         setLayout(new BorderLayout());
 
+        Response<List<Item>> response = controller.getOurItems();
+        ourItems = response.getResult();
+
         showMenu();
         showSubAdd();
         showStock();
+        makeEvent();
         add(menu, BorderLayout.NORTH);
         add(subAddPanel, BorderLayout.CENTER);
         add(stockPanel, BorderLayout.SOUTH);
@@ -43,11 +56,15 @@ public class AdminPanel extends JPanel {
      * 재고 넣고 빼기 생성하기
      */
     private void showStock() {
-        JPanel[] typePanel = new JPanel[7];
+        typePanel = new JPanel[7];
+
         for (int i=0;i<7;i++){
-            typePanel[i]=new JPanel(new GridLayout(2,1));
-            JLabel item = new JLabel(items[i]);
-            JLabel num = new JLabel(counts[i]);
+            typePanel[i] = new JPanel(new GridLayout(2, 1));
+
+            JLabel item = new JLabel(ourItems.get(i).getName());
+            String itemCode = ourItems.get(i).getItemCode();
+            Response<Integer> stockResponse = controller.getItemCount(itemCode);
+            JLabel num = new JLabel(stockResponse.getResult().toString());
             typePanel[i].add(item);
             typePanel[i].add(num);
             stockPanel.add(typePanel[i]);
@@ -59,11 +76,61 @@ public class AdminPanel extends JPanel {
      * -, 개수, +, SUB, ADD 버튼
      */
     private void showSubAdd() {
+        subAddPanel.add(selectedItem);
         subAddPanel.add(minusBtn);
         subAddPanel.add(countLb);
         subAddPanel.add(plusBtn);
         subAddPanel.add(subBtn);
         subAddPanel.add(addBtn);
+    }
+
+    /**
+     * 플러스 마이너스 이벤트 처리
+     */
+    private void makeEvent() {
+        minusBtn.addActionListener(actionEvent -> {
+            if (userSelectionIndex != -1 && userSelectionQuantity > 0) {
+                userSelectionQuantity--;
+                countLb.setText(userSelectionQuantity + "개");
+            }
+        });
+        plusBtn.addActionListener(actionEvent -> {
+            if (userSelectionIndex != -1 && userSelectionQuantity < 999) {
+                userSelectionQuantity++;
+                countLb.setText(userSelectionQuantity + "개");
+            }
+        });
+        subBtn.addActionListener(actionEvent -> {
+            if (userSelectionIndex != -1) {
+                String itemCode = ourItems.get(userSelectionIndex).getItemCode();
+                Response<String> updateResponse = controller.updateStock(itemCode, -userSelectionQuantity);
+                if (updateResponse.isSuccess()) {
+                    JOptionPane.showMessageDialog(null, "재고 감소에 성공했습니다.");
+                } else {
+                    JOptionPane.showMessageDialog(null, "재고 감소에 실패했습니다.");
+                }
+                initSelectedInfo();
+            }
+        });
+        addBtn.addActionListener(actionEvent -> {
+            if (userSelectionIndex != -1) {
+                String itemCode = ourItems.get(userSelectionIndex).getItemCode();
+                Response<String> updateResponse = controller.updateStock(itemCode, userSelectionQuantity);
+                if (updateResponse.isSuccess()) {
+                    JOptionPane.showMessageDialog(null, "재고를 추가에 성공했습니다.");
+                } else {
+                    JOptionPane.showMessageDialog(null, "재고 추가에 실패했습니다.");
+                }
+                initSelectedInfo();
+            }
+        });
+    }
+
+    /**
+     * 재고정보 업데이트
+     */
+    public void updateStockStatus() {
+
     }
 
     /**
@@ -74,9 +141,24 @@ public class AdminPanel extends JPanel {
         menu.setLayout(new GridLayout(1,7));
         for(int i=0;i<7;i++){
             itemsPanel[i]=new JPanel(new GridLayout(2, 1));
-            itemsBtn[i] = new JButton(items[i]);
+            itemsBtn[i] = new JButton(ourItems.get(i).getName());
+            int finalI = i;
+            itemsBtn[i].addActionListener(actionEvent -> {
+                userSelectionIndex = finalI;
+                selectedItem.setText(ourItems.get(finalI).getName());
+            });
             itemsPanel[i].add(itemsBtn[i]);
             menu.add(itemsPanel[i]);
         }
+    }
+
+    /**
+     * 선택 초기화
+     */
+    public void initSelectedInfo() {
+        userSelectionIndex = -1;
+        userSelectionQuantity = 0;
+        selectedItem.setText("");
+        countLb.setText("");
     }
 }
