46c35438ce3b10a46b5b0c7bfc1be072f5435bf3
kyuhyeongkwon <akcps1tp@naver.com>
2022-05-23 11:40:37 +0900

    Add Main

--- src/main/java/dvm/Main.java
+++ src/main/java/dvm/Main.java
@@ -1,22 +1,14 @@
 package dvm;
 
+import dvm.config.AppConfig;
+import dvm.controller.Controller;
+import dvm.gui.MainFrame;
 import dvm.repository.ItemRepository;
 import dvm.service.ItemService;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-
-        // item testing
-        ItemRepository ir = new ItemRepository();
-        ItemService service=new ItemService(ir);
-
-        String itemCode = "01";
-        int qty = 111;
-        System.out.println(service.isEnough(itemCode, qty));
-        System.out.println(service.getItemPrice(itemCode));
-//        System.out.println(service.updateStock(itemCode, qty));
-        System.out.println(service.getItemCount(itemCode));
+        Controller controller = AppConfig.controller();
+        new MainFrame(controller);
     }
 }
