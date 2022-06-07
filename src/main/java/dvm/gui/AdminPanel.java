8106b748dae93fe7e8daf1dedd678353b8e2daed
kyuhyeongkwon <akcps1tp@naver.com>
2022-06-07 22:49:59 +0900

    Rollback AdminPanel to previous versioin

--- src/main/java/dvm/gui/AdminPanel.java
+++ src/main/java/dvm/gui/AdminPanel.java
@@ -3,7 +3,6 @@ package dvm.gui;
 import dvm.controller.Controller;
 import dvm.domain.Item;
 import dvm.domain.Response;
-import dvm.util.Observer;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,7 +11,7 @@ import java.util.List;
 /**
  * 관리자 화면
  */
-public class AdminPanel extends JPanel implements Observer {
+public class AdminPanel extends JPanel {
 
     Controller controller;
     JPanel menu = new JPanel();// 아이템 7개 panel을 담고 있는 panel
@@ -138,7 +137,7 @@ public class AdminPanel extends JPanel implements Observer {
                 Response<String> updateResponse = controller.updateStock(itemCode, userSelectionQuantity);
                 if (updateResponse.isSuccess()) {
                     JOptionPane.showMessageDialog(null, "재고 추가에 성공했습니다.");
-//                    updateStockStatus();
+                    updateStockStatus();
                 } else {
                     JOptionPane.showMessageDialog(null, "재고 추가에 실패했습니다.");
                 }
@@ -164,10 +163,5 @@ public class AdminPanel extends JPanel implements Observer {
         userSelectionQuantity = 0;
         selectedItem.setText("");
         countLb.setText("0개");
-    }
-
-    @Override
-    public void updateObserver(String itemCode, int quantity) {
-        updateStockStatus();
     }
 }
