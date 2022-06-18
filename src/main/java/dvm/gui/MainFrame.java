9f9bf3fbd1d9c4ba3dfbf5cc0e27afe3d97908c4
kyuhyeongkwon <akcps1tp@naver.com>
2022-06-18 18:01:32 +0900

    Fix singleton error

--- src/main/java/dvm/gui/MainFrame.java
+++ src/main/java/dvm/gui/MainFrame.java
@@ -88,7 +88,7 @@ public class MainFrame extends JFrame {
     }
 
     private void addObserver() {
-        AppConfig.itemRepository()
+        ItemRepository.getInstance()
                 .registerObserver((Observer) adminPanel);
     }
 
