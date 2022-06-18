58e88424bdb5ecfab13a8626f3bfcc91267e8956
kyuhyeongkwon <akcps1tp@naver.com>
2022-06-18 21:44:25 +0900

    Remove Redundant object instantiation

--- src/main/java/dvm/Main.java
+++ src/main/java/dvm/Main.java
@@ -3,12 +3,10 @@ package dvm;
 import dvm.config.AppConfig;
 import dvm.controller.Controller;
 import dvm.gui.MainFrame;
-import dvm.repository.ItemRepository;
 
 public class Main {
     public static void main(String[] args) {
         Controller controller = AppConfig.controller();
         MainFrame mainFrame = new MainFrame(controller);
-        ItemRepository itemRepository = ItemRepository.getInstance();
     }
 }
